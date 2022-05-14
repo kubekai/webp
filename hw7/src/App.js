e4af93b06998fcb47a6a3512d7926a7435b70e34
kubekai <100269044+kubekai@users.noreply.github.com>
2022-05-14 21:20:27 +0800

    Add files via upload

---
+++ hw7/src/App.js
@@ -0,0 +1,13 @@
+//import './App.css';
+import Hw7 from './home/Hw7';
+function App() {
+  return (
+    <div className="App">
+      <div >
+       {Hw7()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
