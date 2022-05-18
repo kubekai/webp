a9054902bd9a660fb001ee59da2e811a2574a9d8
kubekai <100269044+kubekai@users.noreply.github.com>
2022-05-18 18:51:51 +0800

    Add files via upload

---
+++ hw8/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Side from'./Side';
+function App() {
+  return (
+    <div className="App">
+      <div>
+      {Side()}
+      </div>
+  
+    </div>
+  );
+}
+
+export default App;
